6ad0b2894f28303e9cce9547382aaa130f0d5513
HAMEER KU <ameerku83@gmail.com>
2024-09-04 08:35:43 +0530

    4/9/24

--- client/src/pages/user/PaymentCancell.jsx
+++ client/src/pages/user/PaymentCancell.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 export const PaymentCancell = () => {
   return (
-    <div>
+    <div className='text-red-700 pt-28'>
          <div>
-        <h1 className=' text-success text-2xl' >Payment failed</h1>
+        <h1 className=' text-2xl ' >Payment failed</h1>
      <button> <Link to={'/user/home'} ></Link> </button>
     </div>
     </div>
--- client/src/pages/user/PaymentSucces.jsx
+++ client/src/pages/user/PaymentSucces.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export const PaymentSucces = () => {
     
   return (
-    <div>
-        <h1 className=' text-success text-2xl' >Payment succes</h1>
+    <div className=' pt-28' >
+        <h1 className=' text-green-700 text-2xl' >Payment succes</h1>
      <button> <Link to={'/user/home'} ></Link> </button>
     </div>
   )
